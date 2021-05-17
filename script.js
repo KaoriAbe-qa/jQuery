b66017c6688ba4429d4f40a390d509d90c3b5d00
Kaori Abe <kaori.abe@rakus-partners.co.jp>
2021-05-17 13:23:05 +0900

    演習２エラー未解消

--- script.js
+++ script.js
@@ -3,6 +3,16 @@
 //演習1
 $(function(){
     $("#btn").on("click", function(){
-        $("#msg").text("阿部佳織");
-    })
+        $("#msg").text("阿部佳織").css("color","blue");
+        $("#msg").toggleClass("on");
+    });
 });
+
+//演習2
+$(function(){
+    $(".on").css("color", "red");      
+});
+
+//演習3
+
+
